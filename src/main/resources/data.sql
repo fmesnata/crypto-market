18b56601697269127b78d077ecf26c6ea8536910
fmesnata <farid.mesnata.pro@gmail.com>
2022-01-05 23:11:09 +0100

    Separate datasource configuration and delete useless SQL scripts

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Cryptocurrency (crypto_id, name, code, price) VALUES (1, 'Bitcoin', 'BTC', 30);
-INSERT INTO Cryptocurrency (crypto_id, name, code, price) VALUES (2, 'Ethereum', 'ETH', 3);
-
-INSERT INTO Account (account_id, owner, balance) VALUES (1, 'Farid', 1000);
